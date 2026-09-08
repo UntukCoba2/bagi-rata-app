3847334fa707efa0c5d3876515aa44dcd7356f58
Daniel SG <hanyauntukcoba2saja@gmail.com>
2026-09-08 11:46:23 +0700

    Upgrade ke PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,40 @@
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+      manifest: {
+        name: 'BagiRata App',
+        short_name: 'BagiRata',
+        description: 'Hitung patungan cerdas, adil, dan tanpa pusing.',
+        theme_color: '#ffffff',
+        background_color: '#f3f4f6', // Warna abu-abu background aplikasi kita
+        display: 'standalone', // INI YANG BIKIN JADI FULLSCREEN (NATIVE APP)
+        orientation: 'portrait',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ]
 })
